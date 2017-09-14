1f2212c2356f3d146dfb2300ab5465fb28c203e7
Daniel Nechala <daniel.nechala@thinkcreatix.com>
2017-09-14 11:35:44 +0000

    Merged in Pill (pull request #1)

--- src/atoms/index.js
+++ src/atoms/index.js
@@ -12,6 +12,7 @@ export {
 export { default as Button } from './Button';
 export { default as CenterView } from './CenterView';
 export { default as FormField } from './FormField';
+export { default as Pill } from './Pill';
 export { default as Screen } from './Screen';
 export { TextField } from 'react-native-material-textfield';
 export { default as TouchableItem } from './TouchableItem';
