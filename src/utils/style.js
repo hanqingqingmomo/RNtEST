ddda67dc524edb6353381d079ae3076b98d88e76
Andrej Badin <contact@andrejbadin.com>
2017-09-22 11:41:56 +0200

    Add `css` utility function. This utility function creates and caches dynamic styles for later reuse. Ideal for text color style, font size styles, ...
    *Usage:*
    ```
    const styleRef: number = css('color', 'red');
    // or
    const styleRef: number = css('fontSize', 10);
    // or
    const styleRef: number = css('backgroundColor', 'blue');
    ```

---
+++ src/utils/style.js
@@ -0,0 +1,14 @@
+// @flow
+
+import { StyleSheet } from 'react-native';
+import memoize from 'lodash.memoize';
+
+const css = (cssProp: string, value: string | number): number => {
+  return StyleSheet.create({ cachedStyle: { [cssProp]: value } }).cachedStyle;
+};
+
+const memoizedCss = memoize(css, (cssProp, value) => {
+  return `cssProp:${cssProp}, value: ${value}`;
+});
+
+export { memoizedCss as css };
