5bbb86430e31cc2747fe9b4d3c2ebf09bb049710
Andrej Badin <contact@andrejbadin.com>
2017-09-25 14:11:51 +0200

    Remove deprecated `blocks/Avatar` component

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,6 +1,5 @@
 // @flow
 
-export { default as Avatar } from './Avatar';
 export { default as LoginForm } from './LoginForm';
 export { default as OfflineStatusOverlay } from './OfflineStatusOverlay';
 export { default as ProfileForm } from './ProfileForm';
