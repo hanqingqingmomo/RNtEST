1d2fdf8c54a750d67f0a609e7ccb4697c109d1f9
Andrej Badin <contact@andrejbadin.com>
2017-10-05 15:22:55 +0200

    Update Bootscreen

--- src/screens/BootScreen.js
+++ src/screens/BootScreen.js
@@ -1,15 +1,31 @@
 // @flow
 
 import React from 'react';
+import { StyleSheet } from 'react-native';
 
-import { ActivityIndicator, CenterView } from '../atoms';
+import { ActivityIndicator } from '../atoms';
+import { LaunchScreenBackground } from '../blocks';
 
-export default class BootScreen extends React.Component {
+export default class LandingScreen extends React.Component {
+  navigate = (
+    route: 'AuthenticationRootScreen' | 'InvitationCodeScreen'
+  ) => () => {
+    this.props.navigation.navigate('');
+    this.props.navigation.navigate(route);
+  };
+
   render() {
     return (
-      <CenterView>
-        <ActivityIndicator />
-      </CenterView>
+      <LaunchScreenBackground>
+        <ActivityIndicator style={styles.activityIndicator} color="white" />
+      </LaunchScreenBackground>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  activityIndicator: {
+    top: '50%',
+    marginTop: 38,
+  },
+});
