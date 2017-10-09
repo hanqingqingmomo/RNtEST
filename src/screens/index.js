8388c19668df848cf274a5f305617aba12981777
Daniel Nechala <daniel.nechala@thinkcreatix.com>
2017-10-09 12:28:10 +0000

    Merged in MemberListScreen (pull request #41)
    MemberListScreen component

--- src/screens/index.js
+++ src/screens/index.js
@@ -11,6 +11,7 @@ export { default as EmailRegistrationScreen } from './EmailRegistrationScreen';
 export { default as InvitationCodeScreen } from './InvitationCodeScreen';
 export { default as InviteFriendsScreen } from './InviteFriendsScreen';
 export { default as LandingScreen } from './LandingScreen';
+export { default as MemberListScreen } from './MemberListScreen';
 export { default as PasswordResetScreen } from './PasswordResetScreen';
 export { default as UserProfileScreen } from './UserProfileScreen';
 export { default as UserSettingsScreen } from './UserSettingsScreen';
