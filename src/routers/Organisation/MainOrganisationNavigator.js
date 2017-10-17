774390a803ac183e736ec268076ce2035ae2731a
Andrej Badin <contact@andrejbadin.com>
2017-10-17 13:18:11 +0200

    Reduce tabs only to ones that should be ready for Demo

--- src/routers/Organisation/MainOrganisationNavigator.js
+++ src/routers/Organisation/MainOrganisationNavigator.js
@@ -3,19 +3,11 @@
 import React from 'react';
 import { TabBarBottom, TabNavigator } from 'react-navigation';
 
-import { CenterView, Icon, Text } from '../../atoms';
+import { Icon } from '../../atoms';
 import OrganisationNewsFeedNavigator from './OrganisationNewsFeedNavigator';
 import CommunitiesNavigator from './CommunitiesNavigator';
-import EventFeedNavigator from './EventFeedNavigator';
+// import EventFeedNavigator from './EventFeedNavigator';
 import { getColor } from '../../utils/color';
-
-function ScreenComponent(props) {
-  return (
-    <CenterView>
-      <Text>{props.children}</Text>
-    </CenterView>
-  );
-}
 
 const makeTabBarIcon = name => iconProps => (
   <Icon name={name} size={24} color={iconProps.tintColor} />
@@ -29,18 +21,18 @@ export default TabNavigator(
         tabBarIcon: makeTabBarIcon('home'),
       }),
     },
-    ConversationsTab: {
-      screen: () => <ScreenComponent>Conversations</ScreenComponent>,
-      navigationOptions: ({ navigation, screenProps }) => ({
-        tabBarIcon: makeTabBarIcon('conversation'),
-      }),
-    },
-    CalendarTab: {
-      screen: EventFeedNavigator,
-      navigationOptions: ({ navigation, screenProps }) => ({
-        tabBarIcon: makeTabBarIcon('calendar'),
-      }),
-    },
+    // ConversationsTab: {
+    //   screen: () => <ScreenComponent>Conversations</ScreenComponent>,
+    //   navigationOptions: ({ navigation, screenProps }) => ({
+    //     tabBarIcon: makeTabBarIcon('conversation'),
+    //   }),
+    // },
+    // CalendarTab: {
+    //   screen: EventFeedNavigator,
+    //   navigationOptions: ({ navigation, screenProps }) => ({
+    //     tabBarIcon: makeTabBarIcon('calendar'),
+    //   }),
+    // },
     CommunitiesTab: {
       screen: CommunitiesNavigator,
       navigationOptions: ({ navigation, screenProps }) => ({
