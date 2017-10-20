dee9513597d28174ec4fc4b21c4c62ee0fc42360
Martin Badin <martin.badin@gmail.com>
2017-10-20 16:28:05 +0200

    fix style for popover

--- src/atoms/Popover.js
+++ src/atoms/Popover.js
@@ -4,7 +4,6 @@ import React from 'react';
 import PopoverTooltip from 'react-native-popover-tooltip';
 import { Dimensions } from 'react-native';
 
-import { Icon } from './index';
 import { getColor } from '../utils/color';
 
 type Props = {
@@ -18,11 +17,26 @@ export default function Popover({ labels, button }: Props) {
       animationType="spring"
       buttonComponent={button}
       items={labels}
-      labelContainerStyle={{
-        backgroundColor: getColor('white'),
-        width: Dimensions.get('window').width,
-      }}
-      tooltipContainerStyle={{ borderRadius: 0 }}
+      labelContainerStyle={styles.labelContainerStyle}
+      tooltipContainerStyle={styles.tooltipContainerStyle}
+      overlayStyle={styles.overlayStyle}
     />
   );
 }
+
+const styles = {
+  tooltipContainerStyle: {
+    borderRadius: 0,
+    padding: 0,
+    margin: 0,
+  },
+  labelContainerStyle: {
+    padding: 0,
+    margin: 0,
+    backgroundColor: getColor('white'),
+    width: Dimensions.get('window').width,
+  },
+  overlayStyle: {
+    backgroundColor: 'rgba(8, 16, 20, 0.4)',
+  },
+};
