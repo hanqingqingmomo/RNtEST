937ce588c85fd209a5512cc77bb90108e16ae472
Martin Badin <martin.badin@gmail.com>
2017-10-20 18:35:32 +0200

    update Like component

--- src/atoms/Like.js
+++ src/atoms/Like.js
@@ -1,30 +1,56 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Fetch, TouchableItem, Count } from '../atoms';
-import { makeLikeRq } from '../utils/requestFactory';
+import { makeLikeRq, makeReadPostReq } from '../utils/requestFactory';
 
 type P = {
   count: number,
   liked: boolean,
   iconName: string,
   objectId: string,
+  disableLink: boolean,
 };
 
-export default function Like({ count, liked, objectId, iconName }: P) {
-  const onPress = (fetch: any) => () => {
-    const likeRq = makeLikeRq(objectId, liked);
-    fetch(likeRq.url, likeRq.options);
+export default class Like extends Component<P> {
+  state = {
+    liked: this.props.liked,
+    count: this.props.count,
   };
 
-  return (
-    <Fetch manual>
-      {({ loading, data, error, fetch }) => (
-        <TouchableItem onPress={onPress(fetch)}>
-          <Count iconName={iconName} count={count} pined={liked} />
-        </TouchableItem>
-      )}
-    </Fetch>
-  );
+  onPress = (fetch: any) => async () => {
+    const { objectId } = this.props;
+    const likeRq = makeLikeRq(objectId, this.state.liked);
+    await fetch(likeRq.url, likeRq.options);
+
+    const readPostReq = makeReadPostReq(objectId);
+    const { data } = await fetch(readPostReq.url, readPostReq.options);
+
+    this.setState({ liked: data.liked, count: data.likes_count });
+  };
+
+  render() {
+    const { iconName, count, disableLink } = this.props;
+
+    return disableLink ? (
+      <Count
+        iconName={iconName}
+        count={this.state.count}
+        pined={this.state.liked}
+      />
+    ) : (
+      <Fetch manual>
+        {({ loading, data, error, fetch }) => (
+          <TouchableItem onPress={this.onPress(fetch)}>
+            <Count
+              iconName={iconName}
+              count={this.state.count}
+              pined={this.state.liked}
+            />
+          </TouchableItem>
+        )}
+      </Fetch>
+    );
+  }
 }
