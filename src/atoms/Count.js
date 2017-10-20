5cc360c3c8df2c62aeab1b5dbf933384e094ae3e
Martin Badin <martin.badin@gmail.com>
2017-10-20 10:59:55 +0200

    add Count component

---
+++ src/atoms/Count.js
@@ -0,0 +1,46 @@
+// @flow
+
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import { Text, Icon, View } from '../atoms';
+import { getColor } from '../utils/color';
+import { css } from '../utils/style';
+import { type IconName } from '../Types';
+
+type P = {
+  pined?: boolean,
+  count: number,
+  iconName: IconName,
+};
+
+export default function Count({ count, iconName, pined }: P) {
+  return (
+    <View style={styles.container}>
+      <Icon
+        name={iconName}
+        size={18}
+        color={pined ? getColor('orange') : '#cfd8dc'}
+        style={styles.icon}
+      />
+      <Text
+        style={css('color', '#8fa3ad')}
+        size={13}
+        lineHeight={18}
+        weight="600"
+      >
+        {count}
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    marginRight: 5,
+  },
+});
--- src/atoms/Like.js
+++ src/atoms/Like.js
@@ -1,11 +1,8 @@
 // @flow
 
 import React from 'react';
-import { StyleSheet } from 'react-native';
 
-import { Fetch, Text, Icon, TouchableItem } from '../atoms';
-import { getColor } from '../utils/color';
-import { css } from '../utils/style';
+import { Fetch, TouchableItem, Count } from '../atoms';
 import { makeLikeRq } from '../utils/requestFactory';
 
 type P = {
@@ -24,33 +21,10 @@ export default function Like({ count, liked, objectId, iconName }: P) {
   return (
     <Fetch manual>
       {({ loading, data, error, fetch }) => (
-        <TouchableItem style={styles.container} onPress={onPress(fetch)}>
-          <Icon
-            name={iconName}
-            size={18}
-            color={liked ? getColor('orange') : '#cfd8dc'}
-            style={[styles.icon]}
-          />
-          <Text
-            style={css('color', '#8fa3ad')}
-            size={13}
-            lineHeight={18}
-            weight="600"
-          >
-            {count}
-          </Text>
+        <TouchableItem onPress={onPress(fetch)}>
+          <Count iconName={iconName} count={count} pined={liked} />
         </TouchableItem>
       )}
     </Fetch>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  icon: {
-    marginRight: 5,
-  },
-});
