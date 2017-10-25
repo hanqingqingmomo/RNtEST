7c569ca1c37d7ceeaeedfc8c3e8ba98c670909dc
Andrej Badin <contact@andrejbadin.com>
2017-10-25 16:47:57 +0200

    Use NewsFeed for Pinned posts screen

--- src/screens/PinnedPostsScreen.js
+++ src/screens/PinnedPostsScreen.js
@@ -1,11 +1,9 @@
 // @flow
 
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
 
-import { Screen, View } from '../atoms';
-import { type Post } from '../Types';
-import NewsFeedList from '../blocks/NewsFeedItem/NewsFeedList';
+import { NewsFeed } from '../blocks';
+import { makeReadPinnedItemsRq } from '../utils/requestFactory';
 
 type P = {
   navigation: any,
@@ -26,58 +24,16 @@ export default class PinnedPostsScreen extends Component<P, S> {
     isBeingUpdated: false,
   };
 
-  requestDelete = (item: Post) => {
-    this.setState({ isBeingDeleted: true });
-    // TODO
-    console.log('request delete', item);
-  };
-
-  deleteSuccessful = (item: Post) => {
-    this.setState({ isBeingDeleted: false });
-    // TODO
-    console.log('delete successful', item);
-  };
-
-  requestUpdate = (item: Post) => {
-    this.setState({ isBeingUpdated: true });
-    // TODO
-    console.log('request update', item);
-  };
-
-  updateSuccessful = (item: Post) => {
-    this.setState({ isBeingUpdated: false });
-    // TODO
-    console.log('update successful', item);
-  };
-
   render() {
-    const { isBeingDeleted, isBeingUpdated } = this.state;
-
+    // TODO do not pass navigation
     return (
-      <Screen>
-        <View style={styles.container}>
-          <NewsFeedList
-            data={this.props.navigation.state.params.data}
-            renderItemProps={{
-              navigation: this.props.navigation,
-              // emitAction: this.emitAction,
-            }}
-            isBeingDeleted={isBeingDeleted}
-            isBeingUpdated={isBeingUpdated}
-            requestDelete={this.requestDelete}
-            requestUpdate={this.requestUpdate}
-            deleteSuccessful={this.deleteSuccessful}
-            updateSuccessful={this.updateSuccessful}
-          />
-        </View>
-      </Screen>
+      <NewsFeed
+        style={{ marginTop: 10 }}
+        navigation={this.props.navigation}
+        request={makeReadPinnedItemsRq(
+          this.props.navigation.state.params.communityId
+        )}
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    paddingVertical: 10,
-    flex: 1,
-  },
-});
