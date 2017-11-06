7f41e177dd8405eabdab0325671d9efcca3de2e3
Martin Badin <martin.badin@gmail.com>
2017-11-06 10:05:22 +0100

    add some flow-typed

---
+++ flow-typed/npm/react-navigation.js
@@ -0,0 +1,7 @@
+declare module 'react-navigation/lib-rn/views/Drawer/DrawerView.js.flow' {
+  declare module.exports: any;
+}
+
+declare module 'react-navigation/lib-rn/TypeDefinition.js.flow' {
+  declare module.exports: any;
+}
