16a6c010938b371a724b8f49195459bb25b32981
Daniel_Nechala <daniel.nechala@thinkcreatixcom>
2017-11-16 14:32:15 +0100

    Android - Logging with Twitter and Facebook

--- android/app/src/main/java/com/app/MainApplication.java
+++ android/app/src/main/java/com/app/MainApplication.java
@@ -16,6 +16,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import io.fullstack.oauth.OAuthManagerPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -40,7 +41,8 @@ public class MainApplication extends Application implements ReactApplication {
         new SplashScreenReactPackage(),
         new ReactNativeConfigPackage(),
         new VectorIconsPackage(),
-        new ImagePickerPackage()
+        new ImagePickerPackage(),
+        new OAuthManagerPackage()
       );
     }
 
--- src/screens/AuthenticationRootScreen.js
+++ src/screens/AuthenticationRootScreen.js
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
+import OAuthManager from 'react-native-oauth';
 
 import { Icon, View, Button, Screen, Text } from '../atoms';
 import { getColor } from '../utils/color';
@@ -31,6 +32,60 @@ export default class AuthenticationRootScreen extends Component<Props> {
     }
   };
 
+  authenticateSocialMediaAccount = (manager, configuration, media) => {
+    manager.configure(configuration);
+    manager
+      .authorize(media)
+      .then(resp => console.log(resp))
+      .catch(err => console.log(err));
+  };
+
+  handleTwitterAuthentication = () => {
+    const manager = new OAuthManager('pba-app');
+    const configuration = {
+      twitter: {
+        consumer_key: 't1vrxymxsalQ84PgD3ew6JZyb',
+        consumer_secret: '0T9Lv4hDR2zrJoxqUMD9QGf2pcKSQ8BqFGUHEbk3dRb6bbB2tB',
+      },
+    };
+
+    this.authenticateSocialMediaAccount(manager, configuration, 'twitter');
+  };
+
+  handleLinkedInAuthentication = () => {
+    const manager = new OAuthManager('pba-test');
+    const configuration = {
+      linkedin: {
+        client_id: '86a6a0objfvxcg',
+        client_secret: 'MMfzum1GG6s7C6GE',
+      },
+    };
+
+    manager.addProvider({
+      linkedin: {
+        auth_version: '2.0',
+        authorize_url: 'https://www.linkedin.com/oauth/v2/authorization',
+        access_token_url: 'https://www.linkedin.com/oauth/v2/accessToken',
+        api_url: 'https://api.linkedin.com/',
+        callback_url: ({ app_name }) => `${app_name}://oauth`,
+      },
+    });
+
+    this.authenticateSocialMediaAccount(manager, configuration, 'linkedin');
+  };
+
+  handleFacebookAuthentication = () => {
+    const manager = new OAuthManager('pba-test');
+    const configuration = {
+      facebook: {
+        client_id: '882238591940046',
+        client_secret: 'a22a0fc8ac0d3a18bc173a68339f610f',
+      },
+    };
+
+    this.authenticateSocialMediaAccount(manager, configuration, 'facebook');
+  };
+
   render() {
     return (
       <Screen fill>
@@ -38,6 +93,27 @@ export default class AuthenticationRootScreen extends Component<Props> {
           <Icon color="orange" name="mpwr-logo" size={64} style={styles.icon} />
 
           <View>
+            <AuthenticationButton
+              color={getColor('facebookBlue')}
+              textColor={getColor('white')}
+              onPress={this.handleFacebookAuthentication}
+              title="Continue with Facebook"
+            />
+
+            <AuthenticationButton
+              color={getColor('linkedinBlue')}
+              textColor={getColor('white')}
+              onPress={this.handleLinkedInAuthentication}
+              title="Continue with LinkedIn"
+            />
+
+            <AuthenticationButton
+              color={getColor('twitterBlue')}
+              textColor={getColor('white')}
+              onPress={this.handleTwitterAuthentication}
+              title="Continue with Twitter"
+            />
+
             <AuthenticationButton
               outline
               color={getColor('orange')}
