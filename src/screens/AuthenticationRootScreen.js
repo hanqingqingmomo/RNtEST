764662a1ef805c0599fd052464ab74b052c335f9
Martin Badin <martin.badin@gmail.com>
2018-01-03 16:29:17 +0100

    added login via facebook and twitter

--- src/screens/AuthenticationRootScreen.js
+++ src/screens/AuthenticationRootScreen.js
@@ -2,13 +2,50 @@
 
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
 import OAuthManager from 'react-native-oauth';
 import LinkedInModal from 'react-native-linkedin';
 
 import { Icon, View, Button, Screen, Text } from '../atoms';
 import { getColor } from '../utils/color';
+import { RQSocialSignIn, RQReadProfile } from '../utils/requestFactory';
 import type { ScreenProps } from '../Types';
-import { css } from '../utils/style';
+import { setUserAccessToken, setUserProfile } from '../redux/ducks/application';
+
+type Provider = 'twitter' | 'facebook';
+
+type Config = {
+  facebook: {
+    client_id: string,
+    client_secret: string,
+  },
+  twitter: {
+    consumer_key: string,
+    consumer_secret: string,
+  },
+};
+
+type Status = string | 'ok';
+
+type Response = {
+  provider?: Provider,
+  response: {
+    authorized: boolean,
+    credentials: {
+      accessToken?: string, // facebook
+      access_token?: string, // twitter
+      authorizationHeader?: string, // facebook
+      access_token_secret?: string, // twitter
+      clientID?: string, // facebook
+      clientSecret?: string, // facebook
+      scopes?: Array<string>, // facebook
+      type?: number, // facebook
+    },
+    identifier: string,
+    uuid: string,
+  },
+  status: Status,
+};
 
 type Route =
   | 'FacebookAuthenticationScreen'
@@ -17,7 +54,26 @@ type Route =
   | 'EmailAuthenticationScreen'
   | 'EmailRegistrationScreen';
 
-type Props = ScreenProps<*>;
+type Props = ScreenProps<*> & {
+  setUserAccessToken: Function,
+  setUserProfile: Function,
+};
+
+type State = {
+  busy: boolean,
+  errors: Array<string>,
+};
+
+const config: Config = {
+  facebook: {
+    client_id: '1252416728196036',
+    client_secret: '9b18fcac2a4fa9d37aa10907dbfc9d51',
+  },
+  twitter: {
+    consumer_key: 't1vrxymxsalQ84PgD3ew6JZyb',
+    consumer_secret: '0T9Lv4hDR2zrJoxqUMD9QGf2pcKSQ8BqFGUHEbk3dRb6bbB2tB',
+  },
+};
 
 function AuthenticationButton(props, otherStyles) {
   return (
@@ -25,7 +81,12 @@ function AuthenticationButton(props, otherStyles) {
   );
 }
 
-export default class AuthenticationRootScreen extends Component<Props> {
+class AuthenticationRootScreen extends Component<Props, State> {
+  state = {
+    busy: false,
+    errors: [],
+  };
+
   navigate = (route: Route) => () => {
     switch (route) {
       case 'EmailAuthenticationScreen':
@@ -36,38 +97,68 @@ export default class AuthenticationRootScreen extends Component<Props> {
     }
   };
 
-  authenticateSocialMediaAccount = (
-    provider: string,
-    configuration: any,
-    scopes?: string
-  ) => {
+  authenticate = async (provider: Provider, resp: Response) => {
+    if (resp.status === 'ok') {
+      const { credentials } = resp.response;
+      const access_token = credentials.accessToken || credentials.access_token;
+
+      const signinResponse = await RQSocialSignIn({ provider, access_token });
+
+      console.log(access_token);
+
+      if (signinResponse.ok) {
+        this.props.setUserAccessToken(signinResponse.data.mobile_token);
+        const profileResponse = await RQReadProfile('me');
+        this.props.setUserProfile(profileResponse.data);
+      } else {
+        this.setState(state => ({
+          errors: state.errors.concat(
+            'Authentication failed. Invalid access token.'
+          ),
+        }));
+      }
+    }
+  };
+
+  authenticateSocialMediaAccount = (provider: Provider, scopes?: string) => {
+    const manager = new OAuthManager('pba-app');
+
     const authorize = () =>
       manager
         .authorize(provider, scopes ? { scopes } : undefined)
-        .then(resp => console.log(`(${provider}) new authorization: `, resp))
+        .then(async (resp: Response) => {
+          console.log('resp', resp);
+          this.authenticate(provider, resp);
+        })
         .catch(err => console.log(err));
 
-    const manager = new OAuthManager('pba-app');
+    manager.configure(config);
 
-    manager.configure(configuration);
     manager
       .savedAccounts()
-      .then(savedAccounts => {
-        console.log(`already authorized against: `, savedAccounts);
-        const isAuthorizedAgainstProvider =
-          savedAccounts.accounts.filter(
-            account => account.provider === provider
-          ).length > 0;
-        if (isAuthorizedAgainstProvider) {
-          console.log('going to deauthorize');
-          manager
-            .deauthorize(provider)
-            .then(deauthorized => {
-              console.log(`after deauthorization: `, deauthorized);
-              console.log('going to authorize');
-              authorize();
-            })
-            .catch(err => console.log(err));
+      .then((savedAccounts: { accounts: Array<Response> }) => {
+        console.log('already authorized against: ', savedAccounts);
+
+        const account = savedAccounts.accounts.find(
+          account => account.provider === provider
+        );
+
+        if (!!account) {
+          console.log('relogin');
+
+          this.authenticate(provider, account);
+
+          // manager
+          //   .deauthorize(provider)
+          //   .then((deauthorized: { status: Status }) => {
+          //     console.log('after deauthorization: ', deauthorized);
+
+          //     if (deauthorized.status === 'ok') {
+          //       console.log('going to authorize');
+          //       authorize();
+          //     }
+          //   })
+          //   .catch(err => console.log(err));
         } else {
           authorize();
         }
@@ -76,24 +167,10 @@ export default class AuthenticationRootScreen extends Component<Props> {
   };
 
   handleTwitterAuthentication = () =>
-    this.authenticateSocialMediaAccount('twitter', {
-      twitter: {
-        consumer_key: 't1vrxymxsalQ84PgD3ew6JZyb',
-        consumer_secret: '0T9Lv4hDR2zrJoxqUMD9QGf2pcKSQ8BqFGUHEbk3dRb6bbB2tB',
-      },
-    });
+    this.authenticateSocialMediaAccount('twitter');
 
   handleFacebookAuthentication = () =>
-    this.authenticateSocialMediaAccount(
-      'facebook',
-      {
-        facebook: {
-          client_id: '1252416728196036',
-          client_secret: '9b18fcac2a4fa9d37aa10907dbfc9d51',
-        },
-      },
-      'public_profile'
-    );
+    this.authenticateSocialMediaAccount('facebook', 'public_profile');
 
   render() {
     return (
@@ -158,6 +235,10 @@ export default class AuthenticationRootScreen extends Component<Props> {
     );
   }
 }
+
+export default connect(null, { setUserAccessToken, setUserProfile })(
+  AuthenticationRootScreen
+);
 
 const styles = StyleSheet.create({
   button: {
--- src/utils/requestFactory.js
+++ src/utils/requestFactory.js
@@ -138,6 +138,9 @@ export function RQSignIn(data: {
 export const RQSignUp = (body: *) =>
   api.post('/v2/members/signup', makeFormData(body, ['profile_photo']));
 
+export const RQSocialSignIn = (body: *) =>
+  api.post('/v2/members/social-login', makeFormData(body));
+
 export const makePasswordResetReq = (email: string) =>
   inject({
     url: buildUrl({ path: '/v2/members/reset_password' }),
