0ab3529ac8ecceffd931afd434f35efe16dedb05
Andrej Badin <contact@andrejbadin.com>
2018-05-10 10:10:11 +0200

    Relink BraintreeApplePay SDK for react-native-payments manually

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+rm -rf "./node_modules";
+yarn cache clean;
+rm -rf "~/.rncache";
+rm -rf "ios/build";
+rm -rf "ios/Pods";
+rm -rf "ios/Carthage/Build";
+yarn install;
+cd ios;
+pod install;
+carthage bootstrap --cache-builds --platform iOS;
+yarn run env:activate
