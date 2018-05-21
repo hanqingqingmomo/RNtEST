a901fabf548ddb0184ef975de2a2b3f1b9c93f68
Martin Badin <martin.badin@gmail.com>
2018-05-21 13:17:02 +0200

    fix style

--- src/atoms/HTML.js
+++ src/atoms/HTML.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react';
 import HTML from 'react-native-render-html';
-import { Linking } from 'react-native';
+import { Linking, Text } from 'react-native';
 
 import { getColor } from '../utils/color';
 
@@ -39,6 +39,9 @@ type Props = {
   renderersProps?: Object,
 };
 
+const COLOR = { color: getColor('gray') };
+const BOLD = { fontWeight: 'bold' };
+
 export default class HTMLText extends Component<Props> {
   openUrl = (url: string) => {
     Linking.canOpenURL(url).then(supported => {
@@ -51,21 +54,37 @@ export default class HTMLText extends Component<Props> {
   render(): React$Node {
     const { renderers, baseFontStyle, ...bag } = this.props;
 
+    if (baseFontStyle) {
+      COLOR.color = baseFontStyle.color || getColor('gray');
+    }
+
     return (
       <HTML
         onLinkPress={(evt, href) => this.openUrl(href)}
+        listsPrefixesRenderers={{
+          ul: () => {
+            return <Text style={{ ...COLOR, ...BOLD }}>&bull; </Text>;
+          },
+          ol: (htmlAttribs, children, convertedCSSStyles, passProps) => {
+            return (
+              <Text style={{ ...COLOR, ...BOLD }}>
+                {`${passProps.index + 1}. `}
+              </Text>
+            );
+          },
+        }}
         tagsStyles={{
           h1: {
+            ...BOLD,
             fontSize: 20,
-            fontWeight: 'bold',
-            marginTop: 20,
             marginBottom: 10,
+            marginTop: 20,
           },
           h2: {
+            ...BOLD,
             fontSize: 18,
-            fontWeight: 'bold',
-            marginTop: 20,
             marginBottom: 10,
+            marginTop: 20,
           },
           ul: {
             marginVertical: 10,
@@ -75,9 +94,9 @@ export default class HTMLText extends Component<Props> {
           },
         }}
         baseFontStyle={{
-          fontSize: 14,
-          color: getColor('gray'),
           ...baseFontStyle,
+          ...COLOR,
+          fontSize: 14,
         }}
         {...bag}
       />
